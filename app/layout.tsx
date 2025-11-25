c551ba495adb240079512b629df5b200a675f584
Rolando-Barbella <rolandobarbella@gmail.com>
2025-11-25 21:08:03 +0000

    yea

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
+    default: 'Rolando Barbella page',
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
@@ -54,7 +54,7 @@ export default function RootLayout({
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+          {/* <Navbar /> */}
           {children}
           <Footer />
           <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
 import { BlogPosts } from 'app/components/posts'
+import { Projects } from 'app/components/projects'
+import Image from 'next/image'
+import profilePic from './img/me.jpg'
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      <div className="flex  gap-6 mb-6">
+        <Image
+          src={profilePic}
+          alt="Rolando Barbella"
+          width={80}
+          height={80}
+          className="p-0.5 rounded-xl border border-gray-200 shadow-sm"
+          priority
+        />
+        <div>
+          <h1 className="text-2xl font-semibold tracking-tighter">
+            Rolando Barbella
+          </h1>
+          <p className="text-sm font-semibold tracking-tighter">Developer</p>
+        </div>
+      </div>
+      <p className="mb-2">
+        {`I'm a software engineer who likes building interesting things. Last few years the foucus has been more on mobile apps ( IOS and Android).
+         Apart from my 9-5 job, I work on side projects while learning new things, which you can see in this site.`}
       </p>
-      <div className="my-8">
+      {/* <div className="my-8">
         <BlogPosts />
+      </div> */}
+      <div className="my-8">
+        <Projects />
       </div>
     </section>
   )
